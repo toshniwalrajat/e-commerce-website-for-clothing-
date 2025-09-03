c01249f411609f73a174293d8b380e21e9de4198
toshniwalrajat <rajattoshniwal2@gmail.com>
2025-09-04 00:02:22 +0530

    Fixed circular import issue in ClientOnlyButton

--- components/ClientOnlyButton.tsx
+++ components/ClientOnlyButton.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ClientOnlyButton from '@/components/ClientOnlyButton';
+"use client";
+
+import React from "react";
 
 type ClientOnlyButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const ClientOnlyButton: React.FC<ClientOnlyButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+  return (
+    <button
+      {...props}
+      className={`relative overflow-hidden px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-300 ${props.className || ""}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default ClientOnlyButton;
